54bd2f09c45f43eca022fe9fe7d4a4c85f6431c3
Avdi Grimm <avdi@avdi.org>
2010-03-14 18:31:29 -0400

    Bundlerified.

---
+++ Gemfile
@@ -0,0 +1,20 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+gem "datamapper", "~> 0.10.2"
+gem "dm-is-list", "~> 0.10.2"
+gem "dm-timestamps", "~> 0.10.2"
+gem "dm-validations", "~> 0.10.2"
+gem "rack", "~> 1.1"
+gem "sinatra", "~> 0.9.6"
+gem "addressable", "~> 2.1"
+gem "link_header", "~> 0.0.5"
+gem "haml", "~> 2.2"
+gem "data_objects", "~> 0.10.1"
+gem "do_sqlite3", "~> 0.10.1"
+
+group :test do
+  gem "nokogiri", "~> 1.4"
+  gem "rspec", "~> 1.3"
+  gem "rack-test", "~> 0.5.3"
+end
--- lib/gem-lovefest.rb
+++ lib/gem-lovefest.rb
@@ -1,8 +1,18 @@
-require 'rubygems'
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 require 'datamapper'
 require 'dm-is-list'
 require 'dm-timestamps'
 require 'dm-validations'
+
 $:.unshift(File.expand_path(File.dirname(__FILE__)))
 require 'gem-lovefest/rubygem'
 require 'gem-lovefest/note'
