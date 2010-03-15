f72261fb4a0f37bd157acea60363651dd96df039
Avdi Grimm <avdi@avdi.org>
2010-03-14 20:36:11 -0400

    Glorious refactoring!

--- lib/gem-lovefest/note.rb
+++ lib/gem-lovefest/note.rb
@@ -1,40 +1,27 @@
+require 'forwardable'
+
 module GemLovefest
   class Note
+    extend Forwardable
     include DataMapper::Resource
 
     belongs_to :rubygem, :child_key => [:gem_name]
     belongs_to :user,    :child_key => [:email_address]
     is :list, :scope => [:gem_name]
 
-    attr_accessor :name
-
     property :email_address, String, :key => true
     property :gem_name,      String, :key => true
     property :comment,       Text
     property :created_at,    DateTime
 
+    def_delegators :user, :name, :name=
+
     validates_format :email_address, :as => :email_address
 
-    before :create do
-      gem = Rubygem.first_or_create(:gem_name => gem_name)
-      unless gem.saved?
-        gem.errors.each do |error|
-          errors.add(:gem_name, error)
-        end
-        throw :halt
-      end
-    end
-
-    before :create do
-      user = User.first_or_create(
-        {:email_address => email_address},
-        {:name          => name})
-      unless user.saved?
-        user.errors.each do |error|
-          errors.add(:email_address, error)
-        end
-        throw :halt
-      end
+    def initialize(*args)
+      super(*args)
+      self.user ||= User.first_or_new(:email_address => email_address)
+      self.rubygem ||= Rubygem.first_or_new(:gem_name => gem_name)
     end
 
   end
